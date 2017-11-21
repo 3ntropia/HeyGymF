bad841602a6aa4c537822bdf3179cb06ff316934
3ntropia <fmartinhuber@gmail.com>
2017-11-20 21:02:55 -0300

    "angularizando"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './login.component';
 import {ClientComponent} from './client.component';
+import {AppComponent} from './app.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', component: AppComponent },
   { path: 'login',  component: LoginComponent },
   { path: 'cliente', component: ClientComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {LoginComponent} from './login.component';
 import {ClientComponent} from './client.component';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {ClientService} from './services/client.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FormsModule,
+    HttpModule,
+    BrowserModule,
     LoginComponent,
     ClientComponent
   ],
   imports: [
-    BrowserModule
   ],
-  providers: [],
+  providers: [
+    ClientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
